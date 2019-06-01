7cfd1d47394a1303458b42e00dc280a4beb025ca
ysn <ysnacrk@gmail.com>
2019-06-01 15:11:01 +0300

    language option and clipboard added

--- translate.py
+++ translate.py
@@ -1,31 +1,57 @@
 from googletrans import Translator
-from tkinter import scrolledtext , Tk , Label , Entry ,Button , INSERT , END
+from tkinter import scrolledtext , Tk , Label , Entry ,Button , INSERT , END 
+from tkinter.ttk import Combobox
+
+class Translate:
+
+    def __init__(self):
+        self.window = Tk()
+        
+        self.window.title("Translate Me")
+        self.window.geometry('680x430')
+
+        self.lbl = Label(self.window, text="Çevirilecek Metin")
+        self.lbl.grid(column=0, row=0)
+
+        self.lbl = Label(self.window, text="Çevirilen Metin")
+        self.lbl.grid(column=1, row=0)
+
+        self.txt = scrolledtext.ScrolledText(self.window,width=40,height=10)
+        self.txt.grid(column=0,row=11)
+        self.txt.insert(INSERT ,self.window.clipboard_get())
+
+        self.txt2 = scrolledtext.ScrolledText(self.window,width=40,height=10)
+        self.txt2.grid(column=1,row=11)
+
+        self.btn = Button(self.window, text="Translate", command=self.clicked)
+        self.btn.grid(column=0, row=12)
+    
+
+        self.combo = Combobox(self.window)
+        self.combo['values']= ("tr", "en")
+        self.combo.current(1) #set the selected item
+
+        self.combo.grid(column=0, row=1)
+
+        self.combo2 = Combobox(self.window)
+        self.combo2['values']= ("tr" , "en")
+        self.combo2.current(1) #set the selected item
+
+        self.combo2.grid(column=1, row=1)
+
+    def clicked(self):
+        self.txt2.delete(1.0,END)
+        self.ret = self.txt.get(1.0 , END)
+        self.trans = Translator()
+        self.s=self.trans.translate(str(self.ret), src=str(self.combo.get()) , dest=str(self.combo2.get()))
+        self.txt2.insert(INSERT, self.s.text)
+    
+    def loop(self):
+        self.window.mainloop()
 
 
-def clicked():
-    txt2.delete(1.0,END)
-    ret = txt.get(1.0 , END)
-    trans = Translator()
-    s=trans.translate(str(ret), src='en' , dest="tr")
-    txt2.insert(INSERT, s.text)
 
-window = Tk()
-window.title("Translate Me")
-window.geometry('680x430')
+if __name__ == '__main__':
+    translate = Translate()
+    translate.loop()
 
-lbl = Label(window, text="Çevirilecek Metin")
-lbl.grid(column=0, row=0)
-
-lbl = Label(window, text="Çevirilen Metin")
-lbl.grid(column=1, row=0)
-
-txt = scrolledtext.ScrolledText(window,width=40,height=10)
-txt.grid(column=0,row=10)
-
-txt2 = scrolledtext.ScrolledText(window,width=40,height=10)
-txt2.grid(column=1,row=10)
-
-btn = Button(window, text="Translate", command=clicked)
-btn.grid(column=0, row=11)
-
-window.mainloop()
